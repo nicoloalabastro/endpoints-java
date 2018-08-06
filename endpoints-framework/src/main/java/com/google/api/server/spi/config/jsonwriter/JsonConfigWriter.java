880ecab5f276dbb2d423f952bf241294f6e04508
Clément Denis <clement@altirnao.com>
2018-08-06 13:00:56 +0200

    Requires both 16px and 32px versions of custom API icons to be set

--- endpoints-framework/src/main/java/com/google/api/server/spi/config/jsonwriter/JsonConfigWriter.java
+++ endpoints-framework/src/main/java/com/google/api/server/spi/config/jsonwriter/JsonConfigWriter.java
@@ -171,7 +171,7 @@ public class JsonConfigWriter implements ApiConfigWriter {
     if (config.getDocumentationLink() != null) {
       root.put("documentation", config.getDocumentationLink());
     }
-    if (config.getIconX16() != null || config.getIconX32() != null) {
+    if (config.getIconX16() != null && config.getIconX32() != null) {
       ObjectNode icons = objectMapper.createObjectNode();
       icons.put("x16", config.getIconX16());
       icons.put("x32", config.getIconX32());
